ca071269097fb07704a96aa7ff97e88a71395d23
PhillypHenning <phillyp.henning@gmail.com>
2025-12-11 15:16:14 -0700

    Add CloudWatch Logs for FigmaProxy application

---
+++ infra/logs.tf
@@ -0,0 +1,10 @@
+resource "aws_cloudwatch_log_group" "figma_proxy" {
+  name              = "/aws/ec2/figma-proxy"
+  retention_in_days = 7
+
+  tags = {
+    Name        = "${local.fully_qualified_name}-logs"
+    Environment = var.target_environment
+    Client      = var.client_name
+  }
+}
